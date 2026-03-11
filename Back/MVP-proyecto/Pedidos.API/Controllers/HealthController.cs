0f414a44fa6c1bf1e48b7396280815d12fcbecc0
nchantre <nelsonfabiancs@hotmail.com>
2026-03-11 13:37:21 -0500

    Configuracion de Microservicios de  • Usuarios • Pedidos • Pagos

---
+++ Back/MVP-proyecto/Pedidos.API/Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pedidos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok("Pedidos service running");
+        }
+
+        [HttpGet("status")]
+        public IActionResult Status()
+        {
+            return Ok(new
+            {
+                service = "Perdidos",
+                status = "ok"
+            });
+        }
+    }
+}
---
+++ Back/MVP-proyecto/User.API/Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace User.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok("Usuarios service running");
+        }
+
+        [HttpGet("status")]
+        public IActionResult Status()
+        {
+            return Ok(new
+            {
+                service = "usuarios",
+                status = "ok"
+            });
+        }
+    }
+}
